12d595562d31e92f1e4d93de0cfff8d7f5a89859
Will Thompson <will@willthompson.co.uk>
2012-05-03 15:50:47 +0100

    old seasonses

---
+++ s3.hs
@@ -0,0 +1,17 @@
+episodes :: [Episode]
+episodes =
+    [
+      Episode "20120212" "Errata"
+        [ Segment "17" "2.15.5" "59.57"
+        ]
+    , Episode "20120219" "Hybrids (with Djuke Veldhuis)"
+        [ Segment "17" "4.45" "60.0"
+        ]
+    , Episode "20120304" "Conspiracy"
+        [ Segment "17" "3.15" "59.55"
+        ]
+    , Episode "20120311" "Steampunk, Shams and Space Cowboys (with Kat Arney)"
+        [ Segment "17" "2.49" "60.01"
+        , Segment "18" "0.10" "0.21"
+        ]
+    ]
