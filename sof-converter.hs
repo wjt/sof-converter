6b65edb4dde9ca7c913d79820fcfb36ab9b41b7c
Will Thompson <will@willthompson.co.uk>
2012-05-03 16:27:47 +0100

    Switch to a json config file

--- sof-converter.hs
+++ sof-converter.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE TemplateHaskell #-}
 -- Usage:
--- • Put all your raw episode files (yyyymmddhh.mp3) into a directory;
--- • Update 'episodes' and 'tagEpisode';
+-- • Put all your raw episode files (yyyymmddhh.mp3) into a directory,
+--   along with an episodes.json file;
+-- • Update 'tagEpisode';
 -- • Run this in that directory. It will put temporary files into 'wip' and
 --   finished items into 'done' below CWD.
 import Control.Monad
@@ -8,17 +10,16 @@ import System.Process
 import System.Exit
 import System.Directory
 
+import qualified Data.ByteString.Lazy.Char8 as C
+import Data.Aeson
+import qualified Data.Aeson.Types as AE
+import Data.Aeson.TH
+import Data.Attoparsec.ByteString.Lazy (parse, eitherResult)
+import Data.Char (toLower)
+
 type Date = String
 type Hour = String
 type Point = String
-
-data Episode =
-    Episode { epDate :: Date
-            , epTitle :: String
-            , epSegments :: [Segment]
-            }
-  deriving
-    Show
 
 data Segment =
     Segment { segHour :: Hour
@@ -28,45 +29,26 @@ data Segment =
   deriving
     Show
 
-episodes :: [Episode]
-episodes =
-    [
-      Episode "20110501" "Comic Book Science (with Ben Valsler)"
-        [ Segment "ThinkRadio Ep1a" "58.06" "EOF"
-        , Segment "ThinkRadio Ep1"   "0.04" "56.57"
-        ]
-    , Episode "20110508" "Doctor Who (with Michael Conterio)"
-        [ Segment "ThinkRadio Ep2a" "58.05" "59.59"
-        , Segment "ThinkRadio Ep2"   "0.06" "57.05"
-        ]
-    , Episode "20110515" "Computers (with Will Thompson)"
-        [ Segment "ThinkRadio Ep3a" "58.00" "EOF"
-        , Segment "ThinkRadio Ep3"   "0.00" "57.36"
-        ]
-    , Episode "20110522" "Fictional Scientist Grudge Match (with Will Thompson)"
-        [ Segment "ThinkRadio Ep4a" "58.25" "60.06"
-        , Segment "ThinkRadio Ep4"   "0.04" "59.01"
-        ]
-    , Episode "20110529" "Zombies (with Frank Swain and Thomas Wooley)" 
-        [ Segment "ThinkRadio - Zombies a" "59.52" "60.03"
-        , Segment "ThinkRadio - Zombies"   "0.00"  "58.43"
-        ]
-    , Episode "20110612" "Time Travel (with Andy Pontzen)"
-        [ Segment "ThinkRadio - Time Travel - Andy Ponztena" "57.29" "60.04"
-        , Segment "ThinkRadio - Time Travel - Andy Ponzten"  "0.00"  "59.04"
-        ]
-    , Episode "20110703" "Journey into the Fourth Dimension (with Prof. Andy Parker)"
-        [ Segment "ThinkRadio - Prof Andy Parker" "0.00" "60.00"
-        ]
-    , Episode "20110710" "Late Victorian Gothic Literature (with Dorée Carrier)"
-        [ Segment "ThinkRadio - Gothic Literatue - Doree-a" "58.12" "60.03"
-        , Segment "ThinkRadio - Gothic Literatue - Doree" "0.03" "59.43"
-        ]
-    , Episode "20110724" "Memory (with Dr Amy Milton)"
-        [ Segment "memorya" "57.07" "60.00"
-        , Segment "memory"  "0.00"  "57.00"
-        ]
-    ]
+$(deriveJSON (map toLower . drop 3) ''Segment)
+
+data Episode =
+    Episode { epDate :: Date
+            , epTitle :: String
+            , epSegments :: [Segment]
+            }
+  deriving
+    Show
+
+$(deriveJSON (map toLower . drop 2) ''Episode)
+
+data Season =
+    Season { seasonNumber :: String
+           , seasonEpisodes :: [Episode]
+           }
+  deriving
+    Show
+
+$(deriveJSON (map toLower . drop 6) ''Season)
 
 systemOrDie cmd args = do
     exitCode <- rawSystem cmd args
@@ -75,8 +57,8 @@ systemOrDie cmd args = do
         ExitFailure _ -> exitWith exitCode
 
 splitOne :: Date -> Segment -> IO FilePath
-splitOne _date s = do
-    let input = segHour s ++ ".mp3"
+splitOne date s = do
+    let input = date ++ segHour s ++ ".mp3"
         outputPattern = "wip/@f"
         output = "wip/" ++ input
 
@@ -116,10 +98,10 @@ glueEpisode e chunks = do
 
 url = "http://www.scienceoffiction.co.uk/"
 
-tagEpisode :: Episode -> Int -> Int -> FilePath -> IO ()
-tagEpisode e i n f = do
+tagEpisode :: String -> Episode -> Int -> Int -> FilePath -> IO ()
+tagEpisode seasonNumber e i n f = do
     let args = [ "--artist", "The Science of Fiction"
-               , "--album", "Season 1"
+               , "--album", "Season " ++ seasonNumber
                , "--song", epTitle e
                , "--year", take 4 (epDate e)
                , "--comment", url
@@ -130,12 +112,24 @@ tagEpisode e i n f = do
 
     systemOrDie "id3tag" args
 
-main = do
-    let n = length episodes
+process :: Season
+        -> IO ()
+process season = do
+    let episodes = seasonEpisodes season
+        n = length episodes
+
     ret <- forM (zip [1..] episodes) $ \(i, e) -> do
         chunks <- splitEpisode e
         glued <- glueEpisode e chunks
-        tagEpisode e i n glued
+        tagEpisode (seasonNumber season) e i n glued
         return glued
 
     mapM_ putStrLn ret
+
+main = do
+    result <- fmap (parse json) $ C.readFile "episodes.json"
+    case eitherResult result of
+        Left e -> error $ "grr: " ++ e
+        Right value -> case AE.parseEither parseJSON value of
+            Left e -> error e
+            Right episodes -> process episodes
